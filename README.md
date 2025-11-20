dd8904b5cf273131991b7ae369195e15292d45b2
Jakob Kohler <schasch.dev@protonmail.com>
2025-11-20 17:29:24 +0100

    Added mode switch

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # moon-phase-display
-A little go program to print out the current moon phase as a visually appealing string to be used in bars like polybar
+A little go program to print out the current moon phase as a visually appealing string to be used in bars like polybar. Will be expanded in the future
--- main.go
+++ main.go
@@ -3,19 +3,57 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/JakobKohler/moon-phase-display/api"
 	"github.com/JakobKohler/moon-phase-display/util"
 )
 
-func main() {
-	client := api.NewClient()
+func getMoonPhase(client api.Client) (string, error) {
 	moonPhase, err := client.FetchMoonPhase()
+
+	if err != nil {
+		return "", err
+	}
+
+	outString := util.BuildPrettyMoonPhaseString(moonPhase)
+	return outString, nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: moon-phase-display <mode> [latitude] [longitude]")
+		return
+	}
+
+	mode := os.Args[1]
+
+	var latitude, longitude string
+
+	if len(os.Args) > 2 {
+		latitude = os.Args[2]
+	}
+	if len(os.Args) > 3 {
+		longitude = os.Args[3]
+	}
+
+	client := api.NewClient()
+
+	var outString string
+	var err error
+
+	switch mode {
+	case "moon":
+		outString, err = getMoonPhase(*client)
+	case "sun":
+		fmt.Printf("Unknown mode: %s\n%s\n", latitude, longitude)
+	default:
+		fmt.Printf("Unknown mode: %s\n", mode)
+	}
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	outString := util.BuildPrettyMoonPhaseString(moonPhase)
 	fmt.Printf(outString)
 }
