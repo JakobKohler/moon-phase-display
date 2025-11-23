cab15108d8583d508ea597997090b66ac57a7906
schasch <schasch.dev@protonmail.com>
2025-11-23 19:50:50 +0100

    sparkles

--- util/moonPhase.go
+++ util/moonPhase.go
@@ -18,3 +18,8 @@ func (m *MoonPhase) GetIcon(phase float64) string {
 	iconIdx := int(math.Round(float64(len(m.Icons)) * phase))
 	return m.Icons[iconIdx]
 }
+
+func (m *MoonPhase) isFullMoon(phase float64) bool {
+	iconIdx := int(math.Round(float64(len(m.Icons)) * phase))
+	return iconIdx == 14 //Full moon icon is displayed
+}
--- util/prettyMoonPhase.go
+++ util/prettyMoonPhase.go
@@ -1,10 +1,14 @@
 package util
 
+const starsColor = "e8c55c"
+
 func BuildPrettyMoonPhaseString(moonPhase float64) string {
 	moonPhaseUtil := NewMoonPhase()
-
 	currentPhaseIcon := moonPhaseUtil.GetIcon(moonPhase)
-	moonPhaseUtil.GetIcon(moonPhase)
+
+	if moonPhaseUtil.isFullMoon(moonPhase) {
+		return "%%{F#" + starsColor + "}\uE370%%{F-} %%{F}" + currentPhaseIcon + "%%{F-}%%{F#" + starsColor + "}\uE370"
+	}
 
 	return currentPhaseIcon
 }
